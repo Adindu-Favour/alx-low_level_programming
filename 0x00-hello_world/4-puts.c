9f9bfd6c872b0c65bca5d844e680d2ea4be005a7
Adindu-Favour <adinduf@gmail.com>
2022-09-08 09:04:03 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/** 
+ * str1[]-prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
+	char str1[] = "Programming is like building a multilingual puzzle;
 
 	puts(str1);
 
