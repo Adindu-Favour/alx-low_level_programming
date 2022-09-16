a378fbdd4b12b30398b3b736dab53c4cc2434c40
Adindu-Favour <adinduf@gmail.com>
2022-09-16 15:09:41 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+
+/**
+ * print_number - like a hello world
+ *
+ * @n: params an integer
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int n1 = 0;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('_');
+	}
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
